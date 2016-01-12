5651eea4be8b883072a9aacd8a06a4ae2bf12ed9
Max-Hu <huzhiwise@gmail.com>
2016-01-12 14:56:46 +0800

    refactor init data function

---
+++ src/main/java/controller/BookMarkInit.java
@@ -0,0 +1,24 @@
+package controller;
+
+import mode.BookMarkManagement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class BookMarkInit extends HttpServlet{
+
+    @Override
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        mode.BookMarkManagement management = new BookMarkManagement();
+        response.getWriter().write(management.getInitBookMarksData());
+    }
+
+}
---
+++ src/main/java/mode/BookMarkManagement.java
@@ -0,0 +1,40 @@
+package mode;
+
+import java.io.*;
+
+public class BookMarkManagement {
+
+    private File file;
+    private String bookMarkPath;
+
+    public BookMarkManagement() {
+        getDataFilePath();
+    }
+
+    public String getInitBookMarksData() throws IOException {
+        InputStreamReader read = new InputStreamReader(new FileInputStream(file),"UTF-8");
+        BufferedReader reader=new BufferedReader(read);
+        String line;
+        String fileContent = "";
+        while ((line = reader.readLine()) != null) {
+            fileContent += line;
+        }
+        reader.close();
+        return fileContent;
+    }
+
+    public void writeBookMarksData(String content)throws IOException{
+        OutputStream os = new FileOutputStream(new File(bookMarkPath));
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(os,"UTF-8");
+        BufferedWriter writer=new BufferedWriter(outputStreamWriter);
+        writer.write(content);
+        writer.flush();
+        writer.close();
+    }
+
+    private void getDataFilePath(){
+        String path = BookMarkManagement.class.getResource("../").getFile().toString();
+        bookMarkPath = path + "bookmarks.json";
+        file = new File(bookMarkPath);
+    }
+}
